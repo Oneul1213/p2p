e9557ad55daea430646a56017e321ca72dd9cbe8
Oneul1213 <toda1213@naver.com>
2024-01-14 02:49:54 +0900

    [feature] 메인 페이지 UI 구성

--- src/views/main/MainPage.vue
+++ src/views/main/MainPage.vue
@@ -1,13 +1,56 @@
 <template>
-    <div>
-        메인
+    <div class="root">
+        <div class="container">
+            <div class="title-wrapper">
+                <p class="title">게시판</p>
+            </div>
+            <div class="button-wrapper">
+                <div class="login" v-if="isLogin">
+                    <p class="login-user">{{ `${userName}님, 안녕하세요` }}</p>
+                    <Button label="게시글 목록" />
+                </div>
+                <div class="login" v-if="!isLogin">
+                    <Button label="로그인" />
+                    <Button label="회원가입" />
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
+import Button from 'primevue/button';
 
+const isLogin = ref(false);
+const userName = ref("홍길동");
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 13.375rem;
+    .title-wrapper {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 20.1875rem;
+    }
 
+    .button-wrapper {
+            .login {
+                width: 100%;
+                height: 100%;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+                gap: 2.4375rem;
+            }
+        }
+}
 </style>
