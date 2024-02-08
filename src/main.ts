05dcfbfa929ab664137f83ffa3ea4dabb4621690
Oneul1213 <toda1213@naver.com>
2024-02-08 21:04:02 +0900

    [comment] msw 관련 주석 추가

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,9 @@ import Primevue from 'primevue/config'
 import ToastService from 'primevue/toastservice'
 
 import { worker } from './mocks/worker'
+
 if (import.meta.env.VITE_ENV_MODE === "local") {
+    // ========== msw 관련 처리 ==========
     worker.start({
         onUnhandledRequest(request, print) {
             // /v1을 포함하지 않는 url은 경고를 표시하지 않음
